ac380881c26ed583b62d2f344c7713a9a75c2032
98005 <980055943@qq.com>
2021-09-05 00:18:57 +0800

    first 添加上下文基类

---
+++ src/main/java/com/jjtref/BaseConText.java
@@ -0,0 +1,12 @@
+package com.jjtref;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 基础上下文类
+ */
+class BaseConText {
+    public List<String> nodeEdNames = new ArrayList();//已经历过的节点
+    public String statusesNames = new String();//节点处理状态路径
+}
